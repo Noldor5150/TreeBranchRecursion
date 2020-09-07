ba1b29a8f4ea06e8b8863eef92a73567ecaf9c2c
Noldor5150 <ruikis.paulius@gmail.com>
2020-09-07 18:48:13 +0300

    Add project files.

---
+++ TreeBranchRecursion/Branch.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TreeBranchRecursion
+{
+    class Branch
+    {
+        public List<Branch> Branches { get; set; }
+    }
+}
---
+++ TreeBranchRecursion/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TreeBranchRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Branch tree = new Branch
+            {
+                Branches = new List<Branch>
+                {
+                    new Branch
+                    {
+                        Branches = new List<Branch>()
+                    },
+                    new Branch
+                    {
+                        Branches = new List<Branch>
+                        {
+                            new Branch
+                            {
+                                Branches = new List<Branch>()
+                            },
+                            new Branch
+                            {
+                                Branches = new List<Branch>()
+                            },
+                            new Branch
+                            {
+                                Branches = new List<Branch>
+                                {
+                                    new Branch 
+                                    {
+                                     Branches  = new List<Branch>() 
+                                    },
+                                    new Branch
+                                    {
+                                     Branches  = new List<Branch>
+                                     {
+                                         new Branch
+                                         {
+                                              Branches  = new List<Branch>()
+                                         },
+                                         new Branch
+                                         {
+                                              Branches  = new List<Branch>
+                                              {
+                                                  new Branch
+                                                  {
+                                                      Branches  = new List<Branch>()
+                                                  }
+                                              }
+                                         },
+                                         new Branch
+                                         {
+                                              Branches  = new List<Branch>()
+                                         }
+
+                                     }
+                                    },
+                                    new Branch
+                                    {
+                                     Branches  = new List<Branch>()
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+            Console.WriteLine(CalculateTreeHeight(tree));
+        }
+
+        static int CalculateTreeHeight(Branch tree, int deepestPoint = 0, int currentDepth = 0)
+        {
+            currentDepth++;
+            if (deepestPoint < currentDepth)
+            {
+                deepestPoint = currentDepth;
+            }
+            foreach (var branch in tree.Branches)
+            {
+                deepestPoint = CalculateTreeHeight(branch, deepestPoint, currentDepth);
+            }
+            return deepestPoint;
+        }
+    }
+
+}
